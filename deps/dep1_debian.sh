c5efac8181277d0a07fabccec7096a355adb1319
dreamerchris <dreamerchris@disroot.org>
2022-06-04 07:56:59 +0300

    added deps and added the node service starter plus vdash start

--- deps/dep1_debian.sh
+++ deps/dep1_debian.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo apt -qq update >/dev/null
-sudo apt -qq install -y snapd build-essential moreutils >/dev/null
+sudo apt -qq install -y snapd build-essential moreutils tree unzip >/dev/null

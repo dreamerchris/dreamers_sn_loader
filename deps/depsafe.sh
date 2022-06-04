0998b537e58eb2a7051b01d43761f152b35cd99e
dreamerchris <dreamerchris@disroot.org>
2022-06-04 08:46:38 +0300

    added deletion of ~/.safe and /usr/local/bin/safe

--- deps/depsafe.sh
+++ deps/depsafe.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+rm -rf /usr/local/bin/safe
+rm -rf ~/.safe
+
 curl -so- https://raw.githubusercontent.com/maidsafe/safe_network/master/resources/scripts/install.sh | bash
 PATH=$PATH:$HOME/.safe/cli
 echo ""

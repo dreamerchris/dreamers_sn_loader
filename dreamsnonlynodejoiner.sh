9c6ae5cf4322ee507de81b8279596c9980611175
dreamerchris <51272727+dreamerchris@users.noreply.github.com>
2022-06-11 15:26:33 +0300

    Update dreamsnonlynodejoiner.sh

--- dreamsnonlynodejoiner.sh
+++ dreamsnonlynodejoiner.sh
@@ -11,11 +11,15 @@ CONFIG_URL=https://nx23255.your-storageshare.de/s/F7e2QaDLNC2z94z/download/dream
 safe networks add $SAFENET "$CONFIG_URL"
 safe networks switch $SAFENET
 
-PATH=$PATH:/$HOME/.safe/cli:$HOME/.cargo/bin 
+PATH=$PATH:/$HOME/.safe/cli:$HOME/.cargo/bin:/usr/local/bin/
 
 DIR1=$HOME/.safe/node/local_node1
 mkdir -p $DIR1
 
-nohup bash -c 'RUST_LOG=sn_node=trace,qp2p=info $HOME/.safe/node/sn_node --local-addr "$LOCAL_IP":12000 --public-addr "$PUBLIC_IP":12000 --skip-auto-port-forwarding --root-dir $DIR1 --log-dir $DIR1' > nohupsafeoutput.txt
+RUST_LOG=sn_node=trace $HOME/.safe/node/sn_node --local-addr "$LOCAL_IP":12000 --public-addr "$PUBLIC_IP":12000 --skip-auto-port-forwarding --root-dir $DIR1 --log-dir $DIR1 & disown
+echo ""
+echo "Launching vdash now!"
+echo ""
+sleep 10
 
 $HOME/.cargo/bin/vdash $HOME/.safe/node/local_node1/sn_node.log

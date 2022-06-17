12abe90fa67a27b6032796a112036e2fdc594d01
dreamerchris <51272727+dreamerchris@users.noreply.github.com>
2022-06-17 05:51:52 +0300

    Update undream.sh

--- undream.sh
+++ undream.sh
@@ -1,12 +1,6 @@
  
 #!/bin/bash
 nohup safe node killall >/dev/null 2>&1
-
-sudo systemctl stop sn_node1.service
-sudo systemctl disable sn_node1.service
-
-sudo systemctl daemon-reload
-sudo systemctl reset-failed
 NUM=1
 while [ -d $HOME/.safe/node/local_node$NUM ]
 do

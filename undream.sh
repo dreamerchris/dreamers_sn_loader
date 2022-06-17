8b47a24f73849cfed26d20b82e10250c8df23f99
dreamerchris <51272727+dreamerchris@users.noreply.github.com>
2022-06-17 05:52:14 +0300

    Update undream.sh

--- undream.sh
+++ undream.sh
@@ -1,9 +1,6 @@
  
 #!/bin/bash
 nohup safe node killall >/dev/null 2>&1
-NUM=1
-while [ -d $HOME/.safe/node/local_node$NUM ]
-do
-rm -rf ~/.safe/node/local_node$NUM
-NUM=$NUM+1
-done
+
+rm -rf ~/.safe/node/local_node*
+
